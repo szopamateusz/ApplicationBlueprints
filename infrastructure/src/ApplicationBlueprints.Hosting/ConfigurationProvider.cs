e10f58d028b7ae1507619864bf98791769941d1f
Mateusz Szopa <github@mateusz-szopa.pl>
2020-06-08 08:08:24 +0200

    Add application hosting blueprints

---
+++ infrastructure/src/ApplicationBlueprints.Hosting/ConfigurationProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ApplicationBlueprints.Hosting
+{
+    public class ConfigurationProvider
+    {
+        public IConfigurationRoot CreateBasicConfiguration(string environmentName, string currentDirectory)
+        {
+            return new ConfigurationBuilder().AddBasicSettings(environmentName, currentDirectory).Build();
+        }
+    }
+}
---
+++ infrastructure/src/ApplicationBlueprints.Hosting/Environment/EnvironmentNameExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationBlueprints.Hosting.Environment
+{
+    public static class EnvironmentNameExtensions
+    {
+        public static bool IsLocal(this string environmentName)
+        {
+            return environmentName.Equals(EnvironmentNameConstants.Local, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static bool IsDevelopment(this string environmentName)
+        {
+            return environmentName.Equals(EnvironmentNameConstants.Development, StringComparison.InvariantCultureIgnoreCase);
+        }
+        public static bool IsTest(this string environmentName)
+        {
+            return environmentName.Equals(EnvironmentNameConstants.Test, StringComparison.InvariantCultureIgnoreCase);
+        }
+        public static bool IsUAT(this string environmentName)
+        {
+            return environmentName.Equals(EnvironmentNameConstants.UAT, StringComparison.InvariantCultureIgnoreCase);
+        }
+        public static bool IsPreProd(this string environmentName)
+        {
+            return environmentName.Equals(EnvironmentNameConstants.PreProd, StringComparison.InvariantCultureIgnoreCase);
+        }
+        public static bool IsProduction(this string environmentName)
+        {
+            return environmentName.Equals(EnvironmentNameConstants.Production, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
