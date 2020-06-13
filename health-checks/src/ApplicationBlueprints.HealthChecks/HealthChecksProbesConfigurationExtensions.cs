4ff1958442239a062dd88ddd2b462edcb2813b28
Mateusz Szopa <github@mateusz-szopa.pl>
2020-06-13 22:17:28 +0200

    Add HealthChecks blueprints

---
+++ health-checks/src/ApplicationBlueprints.HealthChecks/HealthChecksProbesConfigurationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace ApplicationBlueprints.HealthChecks
+{
+    public static class HealthChecksProbesConfigurationExtensions
+    {
+        public static IApplicationBuilder UseHealthChecksProbes(this IApplicationBuilder app)
+        {
+            UseLivenessProbe(app);
+            UseReadinessProbe(app);
+
+            return app;
+        }
+
+        private static void UseLivenessProbe(IApplicationBuilder appBuilder)
+        {
+            appBuilder.UseHealthChecks("/liveness", new HealthCheckOptions
+            {
+                Predicate = r => r.Name.Contains("self", StringComparison.CurrentCultureIgnoreCase),
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        }
+
+        private static void UseReadinessProbe(IApplicationBuilder appBuilder)
+        {
+            appBuilder.UseHealthChecks("/healthz", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        }
+    }
+}
---
+++ health-checks/src/ApplicationBlueprints.HealthChecks/HealthChecksRuleBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ApplicationBlueprints.HealthChecks
+{
+    public static class HealthChecksRuleBuilder
+    {
+        public static IServiceCollection AddInternalServiceHealthCheck(this IServiceCollection services, Uri url, string name)
+        {
+            var serviceDomain = url.Host;
+            var healthCheckUrl = $"{serviceDomain}/healthz";
+
+            services.AddHealthChecks().AddUrlGroup(new Uri(healthCheckUrl), name);
+
+            return services;
+        }
+
+        public static IServiceCollection AddExternalServiceHealthChecks(this IServiceCollection services, Uri url, string name)
+        {
+            services.AddHealthChecks().AddUrlGroup(url, name);
+
+            return services;
+        }
+
+        public static IServiceCollection AddSelfServiceHealthCheck(this IServiceCollection services)
+        {
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy());
+
+            return services;
+        }
+    }
+}
