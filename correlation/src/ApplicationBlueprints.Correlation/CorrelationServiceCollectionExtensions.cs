e02a159d3eef3d821f1ae8718e9e54423db4dd60
Mateusz Szopa <github@mateusz-szopa.pl>
2020-06-13 22:00:11 +0200

    Add correlation blueprints

---
+++ correlation/src/ApplicationBlueprints.Correlation/CorrelationServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ApplicationBlueprints.Correlation
+{
+    public static class CorrelationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCorrelationIdStore(this IServiceCollection services)
+        {
+            services.AddScoped<ICorrelationIdStore, CorrelationIdStore>();
+
+            return services;
+        }
+    }
+}
